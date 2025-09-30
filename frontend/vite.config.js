a9a380a6bbf3e570b320f4ca0476e67b0e8c0cca
lazypandaa <eswarkrishnak2005@email.com>
2025-09-30 14:22:24 +0530

    base_corrected

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,21 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: '0.0.0.0', // This allows external connections
+//     port: 5173,      // Explicitly set the port
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // Add this 'base' property
+  base: '/music/', 
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // This allows external connections
-    port: 5173,      // Explicitly set the port
-  }
 })
